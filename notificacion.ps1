18e9b3139f69c90eb4207eeb38c06d79771f6466
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2022-03-25 06:36:20 -0700

    Update notificacion.ps1

--- notificacion.ps1
+++ notificacion.ps1
@@ -5,5 +5,5 @@ Install-Module -Name BurntToast -RequiredVersion 0.8.5;
 
 mkdir C:\logo\;
 Invoke-WebRequest -Uri https://github.com/JorgeOjedaF/install/raw/main/faronics-logo.png -OutFile C:\logo\faronics-logo.png;
-$ToastHeader = New-BTHeader -Id 1 -Title "Faronics";
+$ToastHeader = New-BTHeader -Id 1 -Title "Faronics"
 New-BurntToastNotification -SnoozeAndDismiss -Header $ToastHeader -AppLogo C:\logo\faronics-logo.png -Text "Aviso de nuevo video", 'Vea este video de Faronics';
