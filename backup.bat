26afd920dedbfb25d9082847c2732a798c5fd582
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2022-06-22 16:28:35 -0700

    Update backup.bat
    quite la /x porque no se permite copiar la informacion de seguridad
    "Security information not supported by destination file system"

--- backup.bat
+++ backup.bat
@@ -3,4 +3,4 @@ REM
 REM Copiamos el archivo win.ini a la carpeta de destino.
 REM
 
-xcopy "C:\Windows\win.ini" "\\vmware-host\Shared Folders\Jorge\Respaldo" /c /d /i /k /q /x /y
+xcopy "C:\Windows\win.ini" "\\vmware-host\Shared Folders\Jorge\Respaldo" /c /d /i /k /q /y
