9dfd80dd553e834509b0fd739085b2f23cd03b7c
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2022-06-23 15:21:35 -0700

    Update backup.bat
    basado en 
    https://www.lifewire.com/xcopy-command-2618103#toc-xcopy--xcopy32
    y
    https://docs.microsoft.com/en-us/windows-server/administration/windows-commands/xcopy

--- backup.bat
+++ backup.bat
@@ -1,16 +1,12 @@
 @ECHO OFF
 
-REM Ejemplo de script para hacer resplados.
-REM Solo copio un archivo, pero este script se puede modificar para respaldar carpetas
+REM EJEMPLO DE SCRIPT DE RESPLADOS:
+REM 1) Se crea un archivo de log con la hora
+REM 2) Se crea carpeta de destino si no existe (El destino debiera ser un servidor de archivos, en este ejemplo se deja en la misma maquina)
+REM 3) Se hace el respaldo y se guarda el resultado en el archivo de log. Se puede agregar el argumento /d para que solo copie si hay cambios
+REM 4) Se escribe la hora en el archivo de log
 
-REM Se crea un log
-ECHO %date% %time% - Inicio script >> C:\respaldo.log
-
-REM Si no existe se crea carpeta
+ECHO %date% %time% - Inicio >> C:\respaldo.log
 IF NOT EXIST C:\CARPETA_RESPALDO\NUL MKDIR C:\CARPETA_RESPALDO\
-
-REM Se respalda
-xcopy C:\Windows\win.ini C:\CARPETA_RESPALDO\win.ini* /c /i /k /q /y >> C:\respaldo.log
-
-REM Fin
-ECHO %date% %time% - Fin script >> C:\respaldo.log
+xcopy C:\Windows\system32\drivers\etc C:\CARPETA_RESPALDO\* /c /e /h /i /k /q /r /s /x /y >> C:\respaldo.log
+ECHO %date% %time% - Fin >> C:\respaldo.log
