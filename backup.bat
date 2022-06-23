c69fdb204f8b56bb5c5e826f23caef3f06715e1c
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2022-06-22 17:00:36 -0700

    Update backup.bat
    agrego un log en archivo C:\test.txt

--- backup.bat
+++ backup.bat
@@ -3,4 +3,4 @@ REM
 REM Copiamos el archivo win.ini a la carpeta de destino.
 REM
 
-xcopy "C:\Windows\win.ini" "C:\Respaldo\win.ini*" /c /d /i /k /q /y
+xcopy "C:\Windows\win.ini" "C:\Respaldo\win.ini*" /c /d /i /k /q /y >> c:\test.txt
