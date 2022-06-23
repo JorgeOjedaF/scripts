0d69d1840f3627f74ac21db4506c9d9b2c16080e
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2022-06-22 17:12:29 -0700

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -6,4 +6,4 @@ REM
 REM Check for the existance of the log file, if it does not exist create it. 
 IF NOT EXIST c:\test.txt ECHO %date% %time% - Se crea el archivo > c:\test.txt
 ECHO %date% %time% - Se ejecuta el script >> c:\test.txt
-xcopy C:\Windows\win.ini C:\Respaldo\win.ini* /c /d /i /k /q /y >> c:\test.txt
+xcopy C:\Windows\win.ini C:\Respaldo\win.ini* /c /i /k /q /y >> c:\test.txt
