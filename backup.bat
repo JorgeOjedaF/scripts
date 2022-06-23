b1be915b855be02c8232e43139113110cf121800
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2022-06-22 17:08:01 -0700

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -4,6 +4,6 @@ REM Copiamos el archivo win.ini a la carpeta de destino.
 REM
 
 REM Check for the existance of the log file, if it does not exist create it. 
-IF NOT EXIST c:\test.txt ECHO %date% %time% - Se crear el archivo > c:\test.txt
-
-xcopy "C:\Windows\win.ini" "C:\Respaldo\win.ini*" /c /d /i /k /q /y >> c:\test.txt
+IF NOT EXIST c:\test.txt ECHO %date% %time% - Se crea el archivo > c:\test.txt
+ECHO %date% %time% - Se ejecuta el script >> c:\test.txt
+xcopy C:\Windows\win.ini C:\Respaldo\win.ini* /c /d /i /k /q /y >> c:\test.txt
