81db3abd4a4f1de7587c688161182047e17ac2cd
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2022-08-05 12:49:55 -0700

    Create uninstallWinUpdate.ps1

---
+++ uninstallWinUpdate.ps1
@@ -0,0 +1,5 @@
+# It is recommended to test the script on a local machine for its purpose and effects. 
+# Faronics will not be responsible for any 
+# damage or loss to the data, operating system or setup based on the effects of the script.
+
+wusa /uninstall /kb:890830 /forcerestart
