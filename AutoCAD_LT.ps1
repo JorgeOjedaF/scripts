69eb64423cbf38bff87bde0eb5885a6dd43b95c8
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2022-09-15 10:59:04 -0700

    Update AutoCAD_LT.ps1

--- AutoCAD_LT.ps1
+++ AutoCAD_LT.ps1
@@ -3,8 +3,7 @@ mkdir C:\DescargaAutoCad;
 mkdir C:\CAD;
 
 # descarga el archivo al computador en carpeta de descarga
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/JorgeOjedaF/install/main/AutoCAD_LT_2023_English_11mb.exe
- -OutFile C:\DescargaAutoCad\AutoCAD_LT_2023_English_11mb.exe;
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/JorgeOjedaF/install/main/AutoCAD_LT_2023_English_11mb.exe -OutFile C:\DescargaAutoCad\AutoCAD_LT_2023_English_11mb.exe;
 
 # ejecuta archivo para descomprimir en la carpeta CAD
 powershell -Command {C:\DescargaAutoCad\AutoCAD_LT_2023_English_11mb.exe -suppresslaunch -d "C:\CAD"}
