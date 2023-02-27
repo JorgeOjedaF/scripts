c04a395c7b16070e9ddbb9529ffb6da409cfc012
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-02-27 10:34:14 -0800

    Create wallpaper.ps1

---
+++ wallpaper.ps1
@@ -0,0 +1,12 @@
+# ruta de la imagen a usar como fondo de pantalla
+$ImagePath = "C:\Images\img1.jpg"
+
+# Informacion del fondo de escritorio del registro del usuario 
+$RegistryPath = "HKCU:\Control Panel\Desktop"
+$WallpaperValue = Get-ItemProperty -Path $RegistryPath -Name WallPaper
+
+# Cambia el wallpaper al cambiar el registro de windows
+Set-ItemProperty -Path $RegistryPath -Name WallPaper -Value $ImagePath
+
+# Refresca para que tome los cambios
+RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters
