dd14f66e2123b7e067c51a8408ad0aaa6e317d3e
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-06-26 14:13:13 -0700

    Update cert-fiep.ps1

--- cert-fiep.ps1
+++ cert-fiep.ps1
@@ -1,6 +1,9 @@
+#create the folder if does not exist
 If(!(test-path -PathType container "C:\FIEP"))
 {
       New-Item -ItemType Directory -Path "C:\FIEP"
 }
+#download the cettificate to that folder
 Invoke-WebRequest -Uri https://raw.githubusercontent.com/JorgeOjedaF/install/main/Certificado%20Rede%20Educacional%20-%20Fortinet.crt -OutFile C:\FIEP\CertFiep.crt;
+#import the certificate into a certificate store
 Import-Certificate -FilePath "C:\FIEP\CertFiep.crt" -CertStoreLocation Cert:\LocalMachine\Root
