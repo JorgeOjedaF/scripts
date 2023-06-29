d1a3ad2a59cca4fdd781d7bc5e10a92039edc29d
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-06-29 16:52:32 -0700

    Create wallpaper3.ps1

---
+++ wallpaper3.ps1
@@ -0,0 +1,21 @@
+# Ruta de la imagen del fondo de pantalla
+$imagenFondo = "C:\wallpaper\img3.jpg"
+
+# Cargar el módulo necesario para cambiar el fondo de pantalla
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Wallpaper {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+
+# Definir constantes para cambiar el fondo de pantalla
+$SPI_SETDESKWALLPAPER = 0x0014
+$SPIF_UPDATEINIFILE = 0x01
+$SPIF_SENDCHANGE = 0x02
+
+# Cambiar el fondo de pantalla
+[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagenFondo, $SPIF_UPDATEINIFILE -bor $SPIF_SENDCHANGE)
