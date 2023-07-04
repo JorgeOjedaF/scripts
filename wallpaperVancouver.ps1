ad56603db8ef8f6abac8ac1364be35d5108cfa48
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-04 16:24:02 -0700

    Update wallpaperVancouver.ps1

--- wallpaperVancouver.ps1
+++ wallpaperVancouver.ps1
@@ -1,7 +1,9 @@
 #cambia el wallpaper
-Set-ItemProperty -Path 'HKCU:\Control Panel\Desktop\' -Name Wallpaper -Value "C:\Wallpaper\vancouver.jpg" -Force
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\" -Name Wallpaper -Value "C:\Wallpaper\vancouver.jpg" -Force
 
 Start-Sleep -s 10
+
+Get-ItemProperty "HKCU:\Control Panel\Desktop\Wallpaper"
 
 #reinicia la computadora para que tome efecto
 Restart-Computer -Force
