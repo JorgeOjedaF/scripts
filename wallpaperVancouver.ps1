b799d8076cbca0056ffed9b5be9b9075dac11f69
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-05 09:06:08 -0700

    Update wallpaperVancouver.ps1

--- wallpaperVancouver.ps1
+++ wallpaperVancouver.ps1
@@ -1,2 +1,10 @@
+#crea una carpeta si no existe
+If(!(test-path -PathType container "C:\wallpaper"))
+{
+      New-Item -ItemType Directory -Path "C:\wallpaper"
+}
+#descarga la imagen a la carpeta wallpaper
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/JorgeOjedaF/install/main/vancouver.jpg -OutFile C:\wallpaper\vancouver.jpg;
+
 #cambia el wallpaper
-Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\" -Name Wallpaper -Value "C:\Wallpaper\vancouver.jpg" -Force
+Set-ItemProperty -Path "HKCU:\Control Panel\Desktop\" -Name Wallpaper -Value "C:\wallpaper\vancouver.jpg" -Force
