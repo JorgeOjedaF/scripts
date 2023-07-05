012cb893ce070ea519a5fd7c6092881f72010694
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-05 15:47:11 -0700

    Update wallpaper.ps1

--- wallpaper.ps1
+++ wallpaper.ps1
@@ -1,16 +1,11 @@
 # ruta de la imagen a usar como fondo de pantalla, concatena el parametro para que quede algo asi: "C:\wallpaper\img1.jpg"
 $ImagePath = "C:\wallpaper\" + $args[0]
 
-# Informacion del fondo de escritorio del registro del usuario 
+# Informacion del fondo de escritorio del registro del usuario actual
 $RegistryPath = "HKCU:\Control Panel\Desktop"
-Get-ItemProperty -Path $RegistryPath -Name WallPaper
 
 # Cambia el wallpaper al cambiar el registro de windows
 Set-ItemProperty -Path $RegistryPath -Name WallPaper -Value $ImagePath
-Get-ItemProperty -Path $RegistryPath -Name WallPaper
 
-# Refresca para que tome los cambios
-RUNDLL32.EXE USER32.DLL,UpdatePerUserSystemParameters
-
-#es necesario reiniciar la computadora para que muestre los cambios
+# es necesario reiniciar la computadora para que muestre los cambios
 Restart-Computer -Force
