55eb68b1621daa2f4fdf458590f198af30fbde4f
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-14 12:41:47 -0700

    Update shortcut-webfaronics.ps1

--- shortcut-webfaronics.ps1
+++ shortcut-webfaronics.ps1
@@ -1,4 +1,4 @@
-#deja un enlace para todos los usuarios al sitio de faronics
+# crea un enlace en el escritorio al sitio de faronics, para todos los usuarios
 $Shell = New-Object -ComObject ("WScript.Shell")
 $Favorite = $Shell.CreateShortcut($env:Public + "\Desktop\faronics.url")
 $Favorite.TargetPath = "https://www.faronics.com/es";
