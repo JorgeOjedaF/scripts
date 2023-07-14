5131d59496fec60b051f440162753da5cdae2001
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-14 13:20:17 -0700

    Update shortcut-delete.ps1
    se corrige error

--- shortcut-delete.ps1
+++ shortcut-delete.ps1
@@ -2,4 +2,4 @@
 # recibe 1 parametro con el nombre del archivo de enlace, por ejemplo: shortcut-delete.ps1 wikipedia
 $Shell = New-Object -ComObject ("WScript.Shell")
 $nombre = $args[0]
-Remove-Item -Path $env:Public + "\Desktop\" + $nombre +".url"
+Remove-Item -Path "$env:Public\Desktop\$nombre.url"
