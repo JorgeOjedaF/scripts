c2047c50b42653a4cb5f01b43af65f110cd5dcec
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-14 12:33:55 -0700

    Update shortcut-website.ps1
    se corrige a la carpeta Public para que sea para todos los usuarios

--- shortcut-website.ps1
+++ shortcut-website.ps1
@@ -1,4 +1,4 @@
 $Shell = New-Object -ComObject ("WScript.Shell")
-$Favorite = $Shell.CreateShortcut($env:USERPROFILE + "\Desktop\faronics.url")
+$Favorite = $Shell.CreateShortcut($env:Public + "\Desktop\faronics.url")
 $Favorite.TargetPath = "https://www.faronics.com/es";
 $Favorite.Save()
