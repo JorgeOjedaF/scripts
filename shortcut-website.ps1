42fb018b33d4f802f8cba947f95940693663bdd1
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-14 12:45:56 -0700

    Update shortcut-website.ps1
    se deja parametrizado

--- shortcut-website.ps1
+++ shortcut-website.ps1
@@ -1,4 +1,8 @@
+# crea un enlace en el escritorio a un sitio web, para todos los usuarios
+# recibe 2 parametros el nombre del enlace y el sitio web, por ejemplo shortcut-website.ps1 faronics https://www.faronics.com/es
 $Shell = New-Object -ComObject ("WScript.Shell")
-$Favorite = $Shell.CreateShortcut($env:Public + "\Desktop\faronics.url")
-$Favorite.TargetPath = "https://www.faronics.com/es";
+$nombre = $args[0]
+$sitio = $args[1]
+$Favorite = $Shell.CreateShortcut($env:Public + "\Desktop\" + $nombre +".url")
+$Favorite.TargetPath = $sitio
 $Favorite.Save()
