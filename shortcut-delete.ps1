71e369ebbb91f1584cb4d9f5da655ebadc99c44a
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-14 13:10:54 -0700

    Create shortcut-delete.ps1

---
+++ shortcut-delete.ps1
@@ -0,0 +1,5 @@
+# borra el enlace del escritorio
+recibe 1 parametro con el nombre del archivo de enlace, por ejemplo: shortcut-delete.ps1 wikipedia
+$Shell = New-Object -ComObject ("WScript.Shell")
+$nombre = $args[0]
+Remove-Item -Path $env:Public + "\Desktop\" + $nombre +".url"
