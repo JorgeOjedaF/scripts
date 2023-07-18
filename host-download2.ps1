f3b1799cdf8d90b56a757ebd297fdded350a731e
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-18 15:56:14 -0700

    Delete host-download2.ps1

--- host-download2.ps1
+++
@@ -1,2 +0,0 @@
-# descarga un archivo hosts con sitios bloquedos y reemplaza el existente
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/JorgeOjedaF/install/main/hosts2 -OutFile C:\Windows\System32\drivers\etc\hosts;
