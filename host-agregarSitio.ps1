dac8f723f787d05d4eb5bbb10c188e03e0575c8b
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-18 12:39:19 -0700

    Update host-agregarSitio.ps1

--- host-agregarSitio.ps1
+++ host-agregarSitio.ps1
@@ -1,1 +1,1 @@
-Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n127.0.0.1`$args[0]" -Force
+Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n127.0.0.1`"+$args[0] -Force
