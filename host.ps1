6b08448d6fbdaab72d524125fd4f525a9edec82e
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-18 15:11:41 -0700

    Update host.ps1

--- host.ps1
+++ host.ps1
@@ -1,1 +1,2 @@
-Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n127.0.0.1`tacme.com" -Force
+# agrega un sitio web al archivo host para bloquerlo, por ejemplo tinder.com
+Add-Content -Path $env:windir\System32\drivers\etc\hosts -Value "`n127.0.0.1 tinder.com" -Force
