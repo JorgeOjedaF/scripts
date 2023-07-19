7f686edfe48cd201ab35142da67b07eece772ff4
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-19 12:07:35 -0700

    Update flutter-download.ps1
    correccion

--- flutter-download.ps1
+++ flutter-download.ps1
@@ -1,3 +1,3 @@
 mkdir C:\src;
 Invoke-WebRequest -Uri https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.10.6-stable.zip -OutFile C:\src\flutter_windows_3.10.6-stable.zip;
-Expand-Archive -Path C:\src\flutter\flutter_windows_3.10.6-stable.zip -DestinationPath C:\src\
+Expand-Archive -Path C:\src\flutter_windows_3.10.6-stable.zip -DestinationPath C:\src\
