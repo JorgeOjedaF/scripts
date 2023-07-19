0e18e41b6a89802086ad2f59509c97e104bf9d51
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-19 13:07:32 -0700

    Update flutter-download.ps1
    correcciones

--- flutter-download.ps1
+++ flutter-download.ps1
@@ -1,3 +1,10 @@
+# creamos carpeta
 mkdir C:\src;
-Invoke-WebRequest -Uri https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.10.6-stable.zip -OutFile C:\src\flutter_windows_3.10.6-stable.zip;
+# descargamos flutter comprimido
+$url = "https://storage.googleapis.com/flutter_infra_release/releases/stable/windows/flutter_windows_3.10.6-stable.zip"
+$output = "C:\src\flutter_windows_3.10.6-stable.zip"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+# descomprimimos el zip
 Expand-Archive -Path C:\src\flutter_windows_3.10.6-stable.zip -DestinationPath C:\src\
+# ejecutamos el bat para agregar al PATH
+& C:\src\flutter_windows_3.10.6-stable\flutter\flutter_console.bat
