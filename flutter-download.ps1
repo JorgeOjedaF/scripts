1f1a3dbe87b0fa948ad7200f71a678b38e92b53e
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-07-19 17:06:04 -0700

    Update flutter-download.ps1
    correcciones

--- flutter-download.ps1
+++ flutter-download.ps1
@@ -7,4 +7,6 @@ $output = "C:\src\flutter_windows_3.10.6-stable.zip"
 # descomprimimos el zip
 Expand-Archive -Path C:\src\flutter_windows_3.10.6-stable.zip -DestinationPath C:\src\
 # agregamos la ruta al PATH
-$Env:PATH += ";C:\src\flutter\bin"
+$env:Path += ";C:\src\flutter\bin"
+# se hace permanente el cambio 
+[Environment]::SetEnvironmentVariable("Path", $env:Path, [System.EnvironmentVariableTarget]::Machine)
