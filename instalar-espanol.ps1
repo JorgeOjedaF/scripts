b5c4be7514e4a1969d11c016e1ef488f32e478e3
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-08-03 09:28:53 -0700

    Create instalar-espanol.ps1

---
+++ instalar-espanol.ps1
@@ -0,0 +1,4 @@
+# Instalamos el paquete de espanol latam y lo define como el idioma predefinido del UI
+Install-Language es-MX -CopyToSettings; 
+# Es necesario reinciar el PC para que el cambio tome efecto
+Restart-Computer -Force
