b1472568e9d8168e47d83217a7e09d8886a1034f
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2023-12-05 16:47:47 -0800

    Update SeculusApagarPCFinDeSemana.ps1
    se simplifica el script

--- SeculusApagarPCFinDeSemana.ps1
+++ SeculusApagarPCFinDeSemana.ps1
@@ -1,56 +1,40 @@
 # Este script crea una tarea programada que apagara la computadora cuando el usuario se loguea un dia Sabado o Domingo
-# La tarea programada, llamada ApagarPCLoginWeekend, ejecuta el script1 Mensaje.ps1, el cual muestra el mensaje al usuario y luego, ejecuta el script ApagaPC.ps1, 
-# El script ApagaPC.ps1 espera un minuto y luego apaga la PC
-# La razon de usar 2 scripts es porque si fuera 1 solo script, el usuario podria detener la ejecución al cerrar la ventana de powershell.
+# La tarea programada, llamada ApagarPCLoginWeekend, ejecuta el script Apagado.ps1, el cual muestra el mensaje al usuario y luego apaga la PC
 # Observacion: 
 # Caso de borde: Si el usuario no cierra la sesion el viernes, podria trabajar el dia sabado porque no ha iniciado sesion. 
 # Para evitar eso, se agrega una segunda tarea programada ApagarPCSabado para que se ejecute cada sabado a las 00:01 horas
 
-# Crea una carpeta (oculta) donde se crean los script Mensaje y ApagaPC
+# Crea una carpeta (oculta) donde se crean el script Mensaje
 $rutaCarpeta = "C:\ScriptFaronics"
-$rutaMensaje = "$rutaCarpeta\Mensaje.ps1"
-$rutaApagaPC = "$rutaCarpeta\ApagaPC.ps1"
+$rutaApagado = "$rutaCarpeta\Apagado.ps1"
 $rutaLogs = "$rutaCarpeta\log.txt"
 $carpeta = New-Item -Path $rutaCarpeta -ItemType Directory -Force
 $carpeta.Attributes = [System.IO.FileAttributes]::Hidden
 
-# Crea el script1 Mensaje.ps1
-$contenidoScript1 = @"
-	"Mensaje.ps1" >> "$rutaLogs"
-	Get-Date | Out-File -FilePath "$rutaLogs" -Encoding UTF8 -Append
-	# Mostrar mensaje al usuario
-	Add-Type -AssemblyName System.Windows.Forms
-	`$mensaje = "Esta computadora se apagara en 1 minuto porque esta prohibido su uso Sabado o Domingo"
-	[System.Windows.Forms.MessageBox]::Show(`$mensaje, "Advertencia", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Exclamation)
-	# Ejecutar el script de apagar la computadora
-	Start-Process PowerShell -ArgumentList "-NoProfile -ExecutionPolicy Bypass -File $rutaApagaPC" -WindowStyle Hidden
-"@
-$contenidoScript1 | Out-File -FilePath $rutaMensaje -Encoding UTF8
-
-# Crea el script2 ApagaPC.ps1
-$contenidoScript2 = @"
-	"ApagaPC.ps1" >> "$rutaLogs"
-	Get-Date | Out-File -FilePath "$rutaLogs" -Encoding UTF8 -Append
+# Crea el script1 Apagado.ps1
+$scriptApagado = @"
+	"Apagado.ps1: $(Get-Date)" >> $rutaLogs
 
 	# Obtiene la fecha actual
 	`$fechaActual = Get-Date
 
 	# Verifica si es fin de semana (sabado o domingo)
-	if (`$fechaActual.DayOfWeek -eq 'Monday' -or `$fechaActual.DayOfWeek -eq 'Sunday') {
-		# Programa el apagado de la computadora despues de un x segundos, x ej 300 segundos = 5 minutos
-		`$tiempoEspera = 60
-		
-		# Espera el tiempo especificado
-		Start-Sleep -Seconds `$tiempoEspera
+	if (`$fechaActual.DayOfWeek -eq 'Tuesday' -or `$fechaActual.DayOfWeek -eq 'Sunday') {
+		# Mostrar mensaje al usuario
+		msg.exe * /TIME:15 "Esta computadora se apagara en 1 minuto porque esta prohibido su uso Sabado o Domingo"
+
+  		# Programa el apagado de la computadora despues de 60 segundos, x ej 300 segundos = 5 minutos
+		Start-Sleep -Seconds 60
 
 		# Apaga la computadora
 		Stop-Computer -Force
 	}
+
 "@
-$contenidoScript2 | Out-File -FilePath $rutaApagaPC -Encoding UTF8
+$scriptApagado | Out-File -FilePath $rutaApagado -Encoding UTF8
 
 # Crea la accion, el trigger (iniciar sesion) y el nombre de la tarea
-$accion = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File $rutaMensaje"
+$accion = New-ScheduledTaskAction -Execute "PowerShell.exe" -Argument "-NoProfile -ExecutionPolicy Bypass -File $rutaApagado"
 $trigger = New-ScheduledTaskTrigger -AtLogon
 $tarea = "ApagarPCLoginWeekend"
 $principal = New-ScheduledTaskPrincipal -UserID "NT AUTHORITY\SYSTEM" -LogonType ServiceAccount -RunLevel Highest
