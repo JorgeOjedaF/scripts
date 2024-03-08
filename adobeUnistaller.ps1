8cfa94bb6bb32589b3c1811cae6eb2f7f2576a3e
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2024-03-07 16:51:55 -0800

    Create adobeUnistaller.ps1

---
+++ adobeUnistaller.ps1
@@ -0,0 +1,8 @@
+# descarga adobeUninstaller y lo ejecuta para borrar todos los productos adobe
+# creamos carpeta
+New-Item -Path "C:\adobeUninstaller" -ItemType Directory -Force
+# descargamos flutter comprimido
+$url = "https://github.com/JorgeOjedaF/install/raw/main/AdobeUninstaller.exe"
+$output = "C:\adobeUninstaller\AdobeUninstaller.exe"
+(New-Object System.Net.WebClient).DownloadFile($url, $output)
+Start-Process -FilePath "C:\adobeUninstaller\AdobeUninstaller.exe" -ArgumentList "--all" -WindowStyle Hidden
