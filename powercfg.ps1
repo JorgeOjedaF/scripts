c7ca5f584ced8dd72ba9ef6ba1f3d598422ff152
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2024-04-16 16:23:56 -0700

    Update powercfg.ps1
    correccion de error

--- powercfg.ps1
+++ powercfg.ps1
@@ -2,4 +2,4 @@
 $newSchemeGuid = powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 
 # Dejamos activo el nuevo plan
-powercfg /setactive $newSchemeGuid
+powercfg -setactive $newSchemeGuid[3..34]
