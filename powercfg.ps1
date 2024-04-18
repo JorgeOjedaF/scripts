154b121ab011f019a2f22e3a442f78df3b532820
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2024-04-17 19:48:11 -0700

    Update powercfg.ps1
    corrección

--- powercfg.ps1
+++ powercfg.ps1
@@ -1,5 +1,7 @@
-# Creamos un nuevo plan de energía (basado en el plan de alto rendimiento el cual tiene la suspensión desactivada)
-$newSchemeGuid = powercfg /duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
+#  Creamos un nuevo plan de energía (basado en el plan de alto rendimiento el cual tiene la suspensión desactivada)
+$newSchemeGuid = powercfg -duplicatescheme e9a42b02-d5df-448d-aa00-03f14749eb61
 
 # Dejamos activo el nuevo plan
-powercfg -setactive $newSchemeGuid[3..34]
+powercfg -setactive $newSchemeGuid.substring(19, 36)
+# Se usa el substring para sacar el ID, porque el string es de la forma 
+# "Power Scheme GUID: f24a93ef-f874-47a3-9dc9-6c23b5210854  (Ultimate Performance)"
