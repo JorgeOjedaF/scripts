69156b285f6a958fd5bd91127410c24769d913dc
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2024-06-24 09:56:26 -0700

    Update disable-snippingtool.ps1

--- disable-snippingtool.ps1
+++ disable-snippingtool.ps1
@@ -1,5 +1,11 @@
-// deshabilita la herramienta de recortes
+# deshabilita la herramienta de recortes o SnippingTool
+
+# Si no existe la ruta, la crea
+if (-not (Test-Path "HKLM:\Software\Policies\Microsoft\TabletPC")) {
+    New-Item -Path "HKLM:\Software\Policies\Microsoft\TabletPC" -Force
+}
+
 New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\TabletPC" -Name "DisableSnippingTool" -Value 1 -PropertyType DWord -Force
 
-// se puede reiniciar la pc para asegurar que tome el cambio
-// Restart-Computer -Force
+# se puede reiniciar la pc para asegurar que tome el cambio
+# Restart-Computer -Force
