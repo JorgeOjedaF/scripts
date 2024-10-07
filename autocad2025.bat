52ae8de5ceb3808e98e246eb51c49547be4f2629
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2024-10-07 13:27:49 -0700

    Create autocad2025.bat

---
+++ autocad2025.bat
@@ -0,0 +1,1 @@
+"\\vmware-host\sharedfolders\Jorge\autocad\image\Installer.exe" -i deploy --offline_mode -q -o "\\vmware-host\sharedfolders\Jorge\autocad\image\Collection.xml" --installer_version "2.9.0.31"
