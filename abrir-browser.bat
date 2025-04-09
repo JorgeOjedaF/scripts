6bcb8164042ab18bfcba81e077f093c67275a33c
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-04-09 15:59:31 -0700

    Update abrir-browser.bat

--- abrir-browser.bat
+++ abrir-browser.bat
@@ -1,4 +1,4 @@
-# abre un sitio web en las computadoras con el navegador por defecto
-# Recibe como parametro el sitio que se desea abrir, por ejemplo http://exam.net  
-# Start http://exam.net
+:: abre un sitio web en las computadoras con el navegador por defecto
+:: Recibe como parametro el sitio que se desea abrir, por ejemplo http://exam.net  
+:: Start http://exam.net
 Start $args[0]
