afc69a800abded72a1b7f0baf324a12c1c95404e
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-06-17 10:57:12 -0700

    Create vray-licencia.ps1

---
+++ vray-licencia.ps1
@@ -0,0 +1,8 @@
+$settings = @{
+    servers = @(@{
+        address = args[0]
+        port = 30304
+    })
+}
+
+$settings | ConvertTo-Json -Depth 3 | Out-File "C:\ProgramData\ChaosGroup\Licensing\settings.json" -Encoding UTF8
