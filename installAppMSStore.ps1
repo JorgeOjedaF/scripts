b862519f16553f43bac05846d23a5ff9cdbc443b
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-07-29 12:46:31 -0700

    Update installAppMSStore.ps1
    se cambia a schtasks porque el script anterior daba errores

--- installAppMSStore.ps1
+++ installAppMSStore.ps1
@@ -1,27 +1,18 @@
-# Instala una app desde MS Store usando winget. 
-# Parametros: Recibe el id de la app de MS Store. Se puede encontrar el id de una app con el comando winget search "NombreApp"
-$AppId = $args[0]
+# ID de la aplicación
 $AppId = "9NBLGGH40F2T"
 
-# Ruta del script
+# Ruta al script que se ejecutará al iniciar sesión
 $ScriptPath = "$env:ProgramData\InstallWithWinget.ps1"
 
 # Contenido del script
-$WingetCommand = @"
+$ScriptContent = @"
 winget install --id $AppId --source msstore --silent --accept-package-agreements --accept-source-agreements
-Unregister-ScheduledTask -TaskName InstallCompleteAnatomy -Confirm:\$false
-Remove-Item -Path '$ScriptPath' -Force
+schtasks /Delete /TN InstallCompleteAnatomy /F
+del `"$ScriptPath`"
 "@
 
-# Guardar el script
-Set-Content -Path $ScriptPath -Value $WingetCommand -Encoding UTF8 -Force
+# Guardar script
+Set-Content -Path $ScriptPath -Value $ScriptContent -Encoding UTF8 -Force
 
-# Crear acción, trigger, principal y configuración de la tarea
-$Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$ScriptPath`""
-$Trigger = New-ScheduledTaskTrigger -AtLogOn
-
-# Crear configuración sin asignar principal (usará el usuario logueado)
-$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
-
-# Registrar tarea (sin principal explícito)
-Register-ScheduledTask -TaskName "InstallCompleteAnatomy" -Action $Action -Trigger $Trigger -Settings $Settings -Force
+# Crear la tarea con schtasks.exe para que se ejecute al iniciar sesión del usuario
+schtasks /Create /TN InstallCompleteAnatomy /TR "powershell.exe -ExecutionPolicy Bypass -WindowStyle Hidden -File `"$ScriptPath`"" /SC ONLOGON /RL HIGHEST /F
