fde72fa1bb20e4f5e1631673dbc7d92ce15d755f
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-07-29 12:40:29 -0700

    Update installAppMSStore.ps1
    se quita INTERACTIVE porque da error

--- installAppMSStore.ps1
+++ installAppMSStore.ps1
@@ -6,7 +6,7 @@ $AppId = "9NBLGGH40F2T"
 # Ruta del script
 $ScriptPath = "$env:ProgramData\InstallWithWinget.ps1"
 
-# Contenido del script que se ejecutará al iniciar sesión
+# Contenido del script
 $WingetCommand = @"
 winget install --id $AppId --source msstore --silent --accept-package-agreements --accept-source-agreements
 Unregister-ScheduledTask -TaskName InstallCompleteAnatomy -Confirm:\$false
@@ -19,8 +19,9 @@ Set-Content -Path $ScriptPath -Value $WingetCommand -Encoding UTF8 -Force
 # Crear acción, trigger, principal y configuración de la tarea
 $Action = New-ScheduledTaskAction -Execute "powershell.exe" -Argument "-WindowStyle Hidden -ExecutionPolicy Bypass -File `"$ScriptPath`""
 $Trigger = New-ScheduledTaskTrigger -AtLogOn
-$Principal = New-ScheduledTaskPrincipal -UserId "INTERACTIVE" -LogonType Interactive -RunLevel Highest
 
-# Registrar la tarea
-Register-ScheduledTask -TaskName "InstallCompleteAnatomy" -Action $Action -Trigger $Trigger -Principal $Principal -Force
+# Crear configuración sin asignar principal (usará el usuario logueado)
+$Settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
 
+# Registrar tarea (sin principal explícito)
+Register-ScheduledTask -TaskName "InstallCompleteAnatomy" -Action $Action -Trigger $Trigger -Settings $Settings -Force
