738fa869e654b6766b5ebf148ccb9f5428698c85
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-08-27 10:00:45 -0700

    Update windows11-readiness.ps1

--- windows11-readiness.ps1
+++ windows11-readiness.ps1
@@ -30,8 +30,8 @@ Write-Host "Secure Boot enabled: $sb" -ForegroundColor ($(if ($sbOK) {'Green'} e
 $tempFile = "$env:TEMP\dxdiag_output.txt"
 Start-Process -FilePath "dxdiag.exe" -ArgumentList "/t $tempFile" -Wait -WindowStyle Hidden
 $dxinfo = Get-Content $tempFile
-$dxVersion = ($dxinfo | Select-String "DirectX Version").ToString().Trim()
-$gpuName = ($dxinfo | Select-String "Card name").ToString().Trim()
+$dxVersion = ($dxinfo | Select-String "DirectX Version").ToString().Split(":")[1].Trim()
+$gpuName = ($dxinfo | Select-String "Card name").ToString().Split(":")[1].Trim()
 
 $gpuOK = $dxVersion -match "12"
 Write-Host "DirectX: $gpuOK - $gpuName - $dxVersion" -ForegroundColor ($(if ($gpuOK) {'Green'} else {'Red'}))
