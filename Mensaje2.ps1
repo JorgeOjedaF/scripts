4c1159fc0be3bd78f8efb197d19519336018b1f8
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-09-08 12:27:12 -0700

    Update Mensaje.ps1
    correccion

--- Mensaje2.ps1
+++ Mensaje2.ps1
@@ -3,3 +3,13 @@
 
 Add-Type -AssemblyName System.Windows.Forms
 [System.Windows.Forms.MessageBox]::Show($args[0])
+
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.MessageBox]::Show(
+    $args[0],
+    "Mensaje desde Faronics Cloud",
+    [System.Windows.Forms.MessageBoxButtons]::OK,
+    [System.Windows.Forms.MessageBoxIcon]::Information,
+    [System.Windows.Forms.MessageBoxDefaultButton]::Button1,
+    [System.Windows.Forms.MessageBoxOptions]::ServiceNotification
+)
