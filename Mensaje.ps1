c505282546cf8175013b70aa482b0a9be7a94c3b
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-09-09 13:42:54 -0700

    Update Mensaje.ps1
    se deja msg para usar en w10.
    (no se puede usar [System.Windows.Forms.MessageBox]::Show porque debe ejecutarse en el usuario conectado actualmente)

--- Mensaje.ps1
+++ Mensaje.ps1
@@ -1,15 +1,2 @@
 # msg solo funciona en windows 10
-# msg.exe * $args[0]
-
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show($args[0])
-
-Add-Type -AssemblyName System.Windows.Forms
-[System.Windows.Forms.MessageBox]::Show(
-    $args[0],
-    "Mensaje desde Faronics Cloud",
-    [System.Windows.Forms.MessageBoxButtons]::OK,
-    [System.Windows.Forms.MessageBoxIcon]::Information,
-    [System.Windows.Forms.MessageBoxDefaultButton]::Button1,
-    [System.Windows.Forms.MessageBoxOptions]::ServiceNotification
-)
+msg.exe * $args[0]
