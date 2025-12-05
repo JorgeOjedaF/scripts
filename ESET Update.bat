10e7fa4d1e41e32d76700169b7b99fb89d74fc6c
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-12-05 14:31:29 -0800

    Update ESET Update.bat

--- ESET Update.bat
+++ ESET Update.bat
@@ -1,4 +1,9 @@
 CD "C:\Program Files\ESET\ESET Security"
+:: obtiene el status actual
 ermm.exe get update-status
+:: envia la orden de actualizar (debe estar habilitado rmm para que funcione)
 ermm.exe start update
+:: espera 5 segundos
+timeout /t 5 /nobreak
+:: obtiene el status actual nuevamente
 ermm.exe get update-status
