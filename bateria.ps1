096968d2ba096ed9ae5cc422913b780bc8c170e0
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-12-11 09:28:49 -0800

    Update bateria.ps1
    se ordena el despliegue de la info

--- bateria.ps1
+++ bateria.ps1
@@ -1,19 +1,18 @@
 Get-CimInstance -ClassName Win32_Battery |
-Select-Object @{Name='Carga%';Expression={$_.EstimatedChargeRemaining}},
-              @{Name='BatteryStatus';Expression={$_.BatteryStatus}},
+Select-Object @{Name='%';Expression={$_.EstimatedChargeRemaining}},
               @{Name='Estado de la bateria';Expression={
                 switch ($_.BatteryStatus) {
-                  1 {'Descargando'}
-                  2 {'Con acceso a CA (no necesariamente cargando)'}
-                  3 {'Completamente cargada'}
-                  4 {'Baja'}
-                  5 {'Crítica'}
-                  6 {'Cargando'}
-                  7 {'Cargando - nivel alto'}
-                  8 {'Cargando - nivel bajo'}
-                  9 {'Cargando - nivel crítico'}
-                  10 {'Indefinido'}
-                  11 {'Parcialmente cargada'}
+                  1 {'Descargando (BatteryStatus:1)'}
+                  2 {'Con acceso a CA, no necesariamente cargando (BatteryStatus:2)'}
+                  3 {'Completamente cargada (BatteryStatus:3)'}
+                  4 {'Baja (BatteryStatus:4)'}
+                  5 {'Crítica (BatteryStatus:5)'}
+                  6 {'Cargando (BatteryStatus:6)'}
+                  7 {'Cargando - nivel alto (BatteryStatus:7)'}
+                  8 {'Cargando - nivel bajo (BatteryStatus:8)'}
+                  9 {'Cargando - nivel crítico (BatteryStatus:9)'}
+                  10 {'Indefinido (BatteryStatus:10)'}
+                  11 {'Parcialmente cargada (BatteryStatus:11)'}
                   default { "Desconocido ($($_.BatteryStatus))" }
                 }
               }}
