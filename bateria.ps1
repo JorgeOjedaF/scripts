a557f170a4c7dc727eb26c6c93e2c2a96cacff16
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2025-12-11 09:23:45 -0800

    Enhance battery status reporting in bateria.ps1
    con texto mas descriptivo

--- bateria.ps1
+++ bateria.ps1
@@ -1,1 +1,20 @@
-Get-CimInstance -ClassName Win32_Battery | Select-Object EstimatedChargeRemaining, BatteryStatus
+Get-CimInstance -ClassName Win32_Battery |
+Select-Object @{Name='Carga%';Expression={$_.EstimatedChargeRemaining}},
+              @{Name='BatteryStatus';Expression={$_.BatteryStatus}},
+              @{Name='Estado de la bateria';Expression={
+                switch ($_.BatteryStatus) {
+                  1 {'Descargando'}
+                  2 {'Con acceso a CA (no necesariamente cargando)'}
+                  3 {'Completamente cargada'}
+                  4 {'Baja'}
+                  5 {'Crítica'}
+                  6 {'Cargando'}
+                  7 {'Cargando - nivel alto'}
+                  8 {'Cargando - nivel bajo'}
+                  9 {'Cargando - nivel crítico'}
+                  10 {'Indefinido'}
+                  11 {'Parcialmente cargada'}
+                  default { "Desconocido ($($_.BatteryStatus))" }
+                }
+              }}
+
