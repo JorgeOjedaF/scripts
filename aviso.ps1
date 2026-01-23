1c9929e3842c89a57091d114404fd39bd8d78e09
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2026-01-23 12:35:01 -0800

    Update aviso.ps1

--- aviso.ps1
+++ aviso.ps1
@@ -3,4 +3,4 @@ $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System"
 New-Item -Path $RegPath -Force | Out-Null
 
 Set-ItemProperty -Path $RegPath -Name "legalnoticecaption" -Type String -Value "Aviso Importante de TI"
-Set-ItemProperty -Path $RegPath -Name "legalnoticetext" -Type String -Value "Recuerde guardar su información en el drive para su respaldo."
+Set-ItemProperty -Path $RegPath -Name "legalnoticetext" -Type String -Value "Recuerde guardar su informacion en el drive para su respaldo."
