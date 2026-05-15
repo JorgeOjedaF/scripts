57d518ed5ec23658253ea133592f85ef4bfc9a36
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2026-05-15 11:51:11 -0700

    Update copiar-carpeta.ps1

--- copiar-carpeta.ps1
+++ copiar-carpeta.ps1
@@ -1,8 +1,8 @@
 # Nombre o IP del servidor
-$Origen = args[0]
+$Origen = $args[0]
 
 # Carpeta destino local
-$Destination = args[1]
+$Destination = $args[1]
 
 # Crear carpeta destino
 if (!(Test-Path $Destination)) {
