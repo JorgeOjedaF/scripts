c62ed244d017bf43d7c1cc2ab8115ad73a81ff90
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2026-05-15 11:31:23 -0700

    Create copiar-carpeta.ps1

---
+++ copiar-carpeta.ps1
@@ -0,0 +1,13 @@
+# Nombre o IP del servidor
+$Origen = args[0]
+
+# Carpeta destino local
+$Destination = args[1]
+
+# Crear carpeta destino
+if (!(Test-Path $Destination)) {
+    New-Item -ItemType Directory -Path $Destination
+}
+
+# Copiar archivos
+Copy-Item "$Origen\*" -Destination $Destination -Recurse -Force
