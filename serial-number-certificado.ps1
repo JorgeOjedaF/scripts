884ad82f63ff8fcbbcccd0fdfde4d28c2ee50fd2
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2026-06-25 17:20:44 -0700

    Update and rename serialnumber-certificado.ps1 to serial-number-certificado.ps1

---
+++ serial-number-certificado.ps1
@@ -0,0 +1,1 @@
+Get-ChildItem "$args[0]" | Get-AuthenticodeSignature | ` Select-Object -Property @{Name=’SignerSerialNumber’;Expression={($_.SignerCertificate.SerialNumber)}}
--- serialnumber-certificado.ps1
+++
@@ -1,1 +0,0 @@
-Get-ChildItem "args[0]" | Get-AuthenticodeSignature | ` Select-Object -Property @{Name=’SignerSerialNumber’;Expression={($_.SignerCertificate.SerialNumber)}}
