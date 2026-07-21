08fc2e272d5cab87f75261218d8b036cd30693ae
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2026-07-21 15:24:47 -0700

    Enable three and four-finger touch gestures

---
+++ touchGestureSetting-on.ps1
@@ -0,0 +1,13 @@
+# Habilita el gesto de 3 y 4 dedos en pantallas tactiles. 
+# Settings → Bluetooth & devices → Touch → Three and four-finger touch gestures.
+
+# Busca en todos los usuarios, para no hacerlo "HKCU" pues el script corre normalmente como SYSTEM ACCOUNT
+Get-ChildItem Registry::HKEY_USERS |
+Where-Object { $_.PSChildName -match '^S-1-5-21-.+-\d+$' } |
+ForEach-Object {
+    $Path = "$($_.PSPath)\Control Panel\Desktop"
+
+    if (Test-Path $Path) {
+        New-ItemProperty -Path $Path -Name "TouchGestureSetting" -Value 1 -PropertyType DWord -Force | Out-Null
+    }
+}
