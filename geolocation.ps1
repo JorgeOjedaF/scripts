4a1449a7c6eb777b0c0075d0aeb4dbe27d478721
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2026-08-26 14:07:57 -0700

    Enhance geolocation.ps1 with usage comments
    Added comments explaining location retrieval and requirements.

--- geolocation.ps1
+++ geolocation.ps1
@@ -1,3 +1,6 @@
+# Se recupera la ubicacion del equipo (latitud,longitud) 
+# Para que esto funcione debe estar activado esta opcion: Settings → Privacy & security → Location → Location services = On
+
 Add-Type -AssemblyName System.Device
 
 $watcher = New-Object System.Device.Location.GeoCoordinateWatcher
@@ -24,6 +27,7 @@ if ($watcher.Status -eq "Ready") {
     else {
         Write-Host "Latitud:   $($location.Latitude)"
         Write-Host "Longitud:  $($location.Longitude)"
+        Write-Host "https://www.google.com/maps/search/?api=1&query=$latitude,$longitude"
     }
 
 }
