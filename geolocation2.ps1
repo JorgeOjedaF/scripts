1de0b9727e12edecf579c171acf9d20c0b6ab88a
JorgeOjedaF <97697971+JorgeOjedaF@users.noreply.github.com>
2026-08-27 16:58:22 -0700

    Enhance geolocation script with detail options
    Added conditional detail display for location services status and waiting status.

--- geolocation2.ps1
+++ geolocation2.ps1
@@ -1,18 +1,20 @@
 # Se recupera la ubicacion del equipo (latitud,longitud) 
 # Para que esto funcione debe estar activado esta opcion: Settings → Privacy & security → Location → Location services = On
+# si le paso un parametro, muestra mas detalles 
+$muestradetalles = ($args.Count -gt 0)
 
 Add-Type -AssemblyName System.Device
 
 # determina si esta habilitado Location en el equipo
 $path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\CapabilityAccessManager\ConsentStore\location"
 $valor = (Get-ItemProperty -Path $path -Name "Value" -ErrorAction SilentlyContinue).Value
-Write-Host "Location Services: $valor"
+if($muestradetalles) { Write-Host "Location Services: $valor" }
 
 # Si no estaba habilitado, se habilita
 if ($valor -ne "Allow") {
     New-Item -Path $path -Force | Out-Null
     Set-ItemProperty -Path $path -Name "Value" -Value "Allow"
-    Write-Host "Location Services no estaba habilitado y se habilito"
+    if($muestradetalles) { Write-Host "Location Services no estaba habilitado y se habilito" }
 }
 
 # Iniciar el servicio de ubicación
@@ -24,13 +26,13 @@ $timeout = 60
 $elapsed = 0
 
 while ($watcher.Status -ne "Ready" -and $elapsed -lt $timeout) {
-    Write-Host "Esperando que servicio location este listo. (Watcher.Status: $($watcher.Status) )"
+    if($muestradetalles) { Write-Host "Esperando que servicio location este listo. (Watcher.Status: $($watcher.Status) )" }
     Start-Sleep -Seconds 1
     $elapsed++
 }
 
 if ($watcher.Status -eq "Ready") {
-    Write-Host "Servicio location (Watcher.Status: $($watcher.Status)"
+    if($muestradetalles) { Write-Host "Servicio location (Watcher.Status: $($watcher.Status))" }
 
     $location = $watcher.Position.Location
 
@@ -38,6 +40,7 @@ if ($watcher.Status -eq "Ready") {
         Write-Host "Windows no tiene una ubicación disponible."
     }
     else {
+        Write-Host "-----------------"
         Write-Host "Latitud:   $($location.Latitude)"
         Write-Host "Longitud:  $($location.Longitude)"
         Write-Host "https://www.google.com/maps/search/?api=1&query=$($location.Latitude),$($location.longitude)"
